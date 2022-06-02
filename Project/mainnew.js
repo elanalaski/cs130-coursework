96d5add9d9fa168b573b08a3e8cb63959a13ae5d
elanalaski <elanalaski2023@.unorthwestern.edu>
2022-06-01 22:20:12 -0500

    HW4

--- Project/mainnew.js
+++ Project/mainnew.js
@@ -22,17 +22,21 @@ const closeModal = () => {
 //     input = document.getElementById("addtask").value;
 // }
 
-
+counter = 1; 
 const displayTask = () => {
 	
-	var x = document.getElementById("addask").value;
-	document.getElementById("userTask").innerHTML = x
+	var x = document.getElementById("addask").value; 
+    var template = ` <div class="row"> <p id="userTask${counter}">${x}</p>
+    <input type="checkbox" class="checkbox" id="Box" onclick="finishTask('userTask${counter}')"></div>`
+    counter++;
+	document.getElementById("Mondaytasks").insertAdjacentHTML("beforeend", template); 
 	console.log(`the value of x is: ${x}`)
 	document.getElementById("myModal").style.display="none";
-	document.getElementById("Box").style.display="block";
+	// document.getElementById("Box").style.display="block";
 }
 
-const finishTask= () => {
-	var x = document.getElementById("addask").value;
-	document.getElementById("userTask").innerHTML = x.strike(); 
-}
+const finishTask= (id) => {
+    console.log(id);
+    var x = document.getElementById(id).innerHTML;
+	document.getElementById(id).innerHTML = x.strike(); 
+    }
