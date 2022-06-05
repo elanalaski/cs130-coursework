29ea86b8d88e1162001484ed00916c1031d7de49
elanalaski <elanalaski2023@.unorthwestern.edu>
2022-06-05 12:03:11 -0500

    Final

--- Project/mainnew.js
+++ Project/mainnew.js
@@ -10,7 +10,8 @@ const openModal = () => {
         document.getElementById("myModal").style.display="block";
         // console.log("working");
         }
-    
+
+
 const closeModal = () => {
     close = document.getElementsByClassName("close")[0];
     document.getElementById("myModal").style.display="none";
@@ -24,19 +25,26 @@ const closeModal = () => {
 
 counter = 1; 
 const displayTask = () => {
-	
+    var select = document.getElementById('Days');
+    var value = select.options[select.selectedIndex].value;
+
+    console.log(value); 
 	var x = document.getElementById("addask").value; 
     var template = ` <div class="row"> <p id="userTask${counter}">${x}</p>
     <input type="checkbox" class="checkbox" id="Box" onclick="finishTask('userTask${counter}')"></div>`
     counter++;
-	document.getElementById("Mondaytasks").insertAdjacentHTML("beforeend", template); 
+	document.getElementById(`${value}tasks`).insertAdjacentHTML("beforeend", template);
+    console.log(document.querySelector("select.option.value"));
 	console.log(`the value of x is: ${x}`)
 	document.getElementById("myModal").style.display="none";
 	// document.getElementById("Box").style.display="block";
+
 }
 
+counter2=0;
 const finishTask= (id) => {
     console.log(id);
     var x = document.getElementById(id).innerHTML;
-	document.getElementById(id).innerHTML = x.strike(); 
+        document.getElementById(id).innerHTML = x.strike(); 
+
     }
